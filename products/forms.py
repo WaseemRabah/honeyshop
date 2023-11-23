eed6bb98a88ebd11e9b9bc7deedcd2d92431e4e9
waseem <waseem.96.ra@gmail.com>
2023-11-23 12:12:54 +0100

    Edit product form

--- products/forms.py
+++ products/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'quantity']
+        fields = ['name', 'description', 'price', 'quantity', 'category', 'image']

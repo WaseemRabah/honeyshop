c87109c49912e287e418d4dd6981e9b15bcc5d2b
waseem <waseem.96.ra@gmail.com>
2023-11-23 16:07:12 +0100

    Create home view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+class HomeView(View):
+    template_name = 'home/home.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)

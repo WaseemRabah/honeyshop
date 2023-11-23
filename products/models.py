a893d9edbcb0f9aa59ba5ea08a5a5279f689f084
waseem <waseem.96.ra@gmail.com>
2023-11-23 11:25:53 +0100

    Edit urls.py

--- products/models.py
+++ products/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Meta:
+    app_label = 'products'
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ProductListView, ProductDetailView
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='product_list'),
+    path('<slug:category_slug>/', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+]

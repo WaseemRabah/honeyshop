501c0598627c3540e24243a7a2fa6dc30dfed2bb
waseem <waseem.96.ra@gmail.com>
2023-11-23 12:38:37 +0100

    Create wishlist view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Wishlist, WishlistItem
+from products.models import Product
+from django.urls import reverse_lazy
 
-# Create your views here.
+class WishlistListView(LoginRequiredMixin, ListView):
+    model = Wishlist
+    template_name = 'wishlist/wishlist_list.html'
+    context_object_name = 'wishlists'
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(user=self.request.user)
+
+class WishlistDetailView(LoginRequiredMixin, DetailView):
+    model = Wishlist
+    template_name = 'wishlist/wishlist_detail.html'
+    context_object_name = 'wishlist'
+
+@login_required
+def add_to_wishlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+    WishlistItem.objects.create(wishlist=wishlist, product=product)
+    return redirect('wishlist:wishlist-list')
+
+class WishlistCreateView(LoginRequiredMixin, CreateView):
+    model = Wishlist
+    template_name = 'wishlist/wishlist_form.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('wishlist:wishlist-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)

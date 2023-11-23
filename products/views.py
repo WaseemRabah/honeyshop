bb6a49ca07e71551896e4804c1c2a5501e50fc21
waseem <waseem.96.ra@gmail.com>
2023-11-23 10:50:27 +0100

    creatin views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import ListView, DetailView
+from .models import Category, Product
 
-# Create your views here.
+class ProductListView(ListView):
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    model = Product
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            return Product.objects.filter(category=category, stock__gt=0)
+        else:
+            return Product.objects.filter(stock__gt=0)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['category'] = get_object_or_404(Category, slug=self.kwargs.get('category_slug', ''))
+        return context
+
+class ProductDetailView(DetailView):
+    template_name = 'products/product_detail.html'
+    model = Product
+    context_object_name = 'product'
+
+    def get_queryset(self):
+        return Product.objects.filter(stock__gt=0)

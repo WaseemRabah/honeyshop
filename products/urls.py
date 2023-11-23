ee6136e0a3efb0e97ca1f091608d87107a1c6577
waseem <waseem.96.ra@gmail.com>
2023-11-23 12:08:00 +0100

    Edit products urls

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'products'
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<slug:category_slug>/', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/add/', ProductCreateView.as_view(), name='product_create'),
+    path('products/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]

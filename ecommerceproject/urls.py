e8a05bccd734350b6c841c462c908f9675692548
waseem <waseem.96.ra@gmail.com>
2023-11-23 16:15:22 +0100

    Create home template

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
     path('wishlist/', include('wishlist.urls')),
+    path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('allauth.account.urls')),
 ]

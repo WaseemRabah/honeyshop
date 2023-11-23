5a4ce262bfcb9aaeaecff38fa1fe73e854f46d7d
waseem <waseem.96.ra@gmail.com>
2023-11-23 12:33:27 +0100

    Create wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="My Wishlist")
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} in {self.wishlist.user.username}'s Wishlist"

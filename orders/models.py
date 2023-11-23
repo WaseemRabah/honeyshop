19e0572db8ad796f5306653a79c0ddd836880d1a
waseem <waseem.96.ra@gmail.com>
2023-11-23 12:24:01 +0100

    Create orders model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)

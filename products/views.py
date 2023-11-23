04b77a25e5c28fa48c098a50ce100c9feedd4f7b
waseem <waseem.96.ra@gmail.com>
2023-11-23 11:33:03 +0100

    Edit productlistview

--- products/views.py
+++ products/views.py
@@ -18,8 +18,13 @@ class ProductListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        context['category'] = get_object_or_404(Category, slug=self.kwargs.get('category_slug', ''))
+        category_slug = self.kwargs.get('category_slug')
+        
+        if category_slug:
+            context['category'] = get_object_or_404(Category, slug=category_slug)
+        else:
+            context['category'] = None
+
         return context
 
 class ProductDetailView(DetailView):

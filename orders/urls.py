1289eaab232301128e19dc4a1a25ff0330717c55
waseem <waseem.96.ra@gmail.com>
2023-11-23 12:26:56 +0100

    Create orders view

---
+++ orders/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import OrderDetailView
+
+urlpatterns = [
+    path('order/<int:order_id>/', OrderDetailView.as_view(), name='order_detail'),
+]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Order
 
-# Create your views here.
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'orders/order_detail.html'
+    context_object_name = 'order'
+    pk_url_kwarg = 'order_id'

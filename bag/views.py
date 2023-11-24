33983520677492c51cd6e941b69fed5b0d017e44
waseem <waseem.96.ra@gmail.com>
2023-11-24 06:47:30 +0100

    Add bag views

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .contexts import Cart
+from products.models import Product
+from orders.models import Order
 
-# Create your views here.
+@login_required
+@require_POST
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product)
+    return redirect('bag:cart')
+
+@login_required
+@require_POST
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('bag:cart')
+
+@login_required
+def view_cart(request):
+    cart = Cart(request)
+    return render(request, 'bag/cart.html', {'cart': cart})
+
+@login_required
+def make_order(request):
+    cart = Cart(request)
+    order = Order.objects.create(user=request.user)
+    for item in cart:
+        order.items.add(item.product, quantity=item.quantity)
+    cart.clear()
+    return render(request, 'bag/order_confirmation.html', {'order': order})

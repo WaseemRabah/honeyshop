15312864881eb762c8e11bc2b4ecb9d1ec26d1c4
waseem <waseem.96.ra@gmail.com>
2023-11-24 07:17:40 +0100

    Add bag app url

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('orders/', include(('orders.urls', 'orders'))),
     path('wishlist/', include('wishlist.urls')),
+    path('bag/', include('bag.urls', namespace='bag')),
     path('checkout/', include('checkout.urls', namespace='checkout')),
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),

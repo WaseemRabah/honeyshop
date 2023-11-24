27e748f572947ff0033c7ab7c431d5d8e0a7f6fd
waseem <waseem.96.ra@gmail.com>
2023-11-24 11:02:39 +0100

    Update wishlist views

--- wishlist/views.py
+++ wishlist/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from .models import Wishlist, WishlistItem
 from products.models import Product
 from django.urls import reverse_lazy
+from orders.models import Order
 
 class WishlistListView(LoginRequiredMixin, ListView):
     model = Wishlist
@@ -30,6 +31,18 @@ def add_to_wishlist(request, product_id):
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
     WishlistItem.objects.create(wishlist=wishlist, product=product)
     return redirect('wishlist:wishlist-list')
+
+@login_required
+def add_wishlist_to_cart(request, wishlist_id):
+    wishlist = get_object_or_404(Wishlist, id=wishlist_id)
+    cart, created = Order.objects.get_or_create(user=request.user, ordered=False)
+
+    for wishlist_item in wishlist.items.all():
+        cart.items.add(product=wishlist_item.product, quantity=1)
+
+    wishlist.items.update(added_to_cart=True)
+
+    return redirect('bag:cart')  #app url
 
 class WishlistCreateView(LoginRequiredMixin, CreateView):
     model = Wishlist

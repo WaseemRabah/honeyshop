583c659d8963995ade20a5f842fa1a29d6f04535
waseem <waseem.96.ra@gmail.com>
2023-11-24 06:49:01 +0100

    Add bag urls

---
+++ bag/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('add/<int:product_id>/', views.add_to_cart, name='add-to-cart'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove-from-cart'),
+    path('cart/', views.view_cart, name='cart'),
+    path('make-order/', views.make_order, name='make-order'),
+]

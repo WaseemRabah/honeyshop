7c8a2610689d1fb9f93d3bd8483fd91b2434637b
waseem <waseem.96.ra@gmail.com>
2023-11-25 11:05:49 +0100

    Setup development environment

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -6,6 +6,8 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -17,9 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--ldd-!-y+1*lw_g(-s+fw7po1+6ucqvme_r%jfzt71d2qe5*k+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['honeyshop-ffba4d388b9e.herokuapp.com']
+if development:
+    ALLOWED_HOSTS = ['127.0.0.1']
+
+else:
+    ALLOWED_HOSTS = ['honeyshop-ffba4d388b9e.herokuapp.com']
 
 
 # Application definition
@@ -83,16 +89,18 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+else:
+    DATABASES = {
+        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation

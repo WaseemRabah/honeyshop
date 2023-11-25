a811e0170c6d6d3ebb288a7238fddb7b602887cf
waseem <waseem.96.ra@gmail.com>
2023-11-25 11:34:45 +0100

    Add order number to the order model

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from products.models import Product
 from django.contrib.auth.models import User
+import uuid
+
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='OrderItem')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    order_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def save(self, *args, **kwargs):
+        # Generate a unique order number before saving the order
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save(*args, **kwargs)
+
+    def generate_order_number(self):
+        # Generate a unique order number based on the current timestamp
+        timestamp_str = self.created_at.strftime('%Y%m%d%H%M%S')
+        user_id_str = str(self.user.id).zfill(5) 
+        return f'{timestamp_str}-{user_id_str}'
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)

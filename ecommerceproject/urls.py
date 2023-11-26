aac17250bb2940086c451639f5ee13c3e72aeb19
waseem <waseem.96.ra@gmail.com>
2023-11-26 10:43:09 +0100

    Add blocks to base template

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +14,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('allauth.account.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

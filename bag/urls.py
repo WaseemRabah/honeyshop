7acbad3df1b36713ad895f945c26eb2b58cf8f45
waseem <waseem.96.ra@gmail.com>
2023-11-30 08:13:57 +0100

    Add add functionality

--- bag/urls.py
+++ bag/urls.py
@@ -5,8 +5,9 @@ from .views import CartView, view_cart
 
 
 urlpatterns = [
-    path('add/<int:product_id>/', views.add_to_cart, name='add-to-cart'),
+    path('add_to_bag/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
     path('remove/<int:product_id>/', views.remove_from_cart, name='remove-from-cart'),
     path('view-cart/', view_cart, name='view-cart'),
     path('make-order/', views.make_order, name='make-order'),
+    path('cart/', views.view_cart, name='cart'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -8,11 +8,22 @@ from django.views.generic import TemplateView
 
 @login_required
 @require_POST
-def add_to_cart(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.add(product=product)
-    return redirect('bag:cart')
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
+
 
 @login_required
 @require_POST

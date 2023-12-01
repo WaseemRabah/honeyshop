bf76cd175e80bb3a5bf9c4c0e22ca57aa2791091
waseem <waseem.96.ra@gmail.com>
2023-12-01 13:56:45 +0100

    Add stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,11 @@ class CheckoutView(View):
             return redirect(reverse('products'))
 
         order_form = OrderForm()
-        context = {'order_form': order_form}
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': 'pk_test_51OIVsBAdWnDYkhgpGaz1IaZCbt3liEImkER1rWpa3ObJp4ykJxSnREprsRNXt8ZIerOcnGcZ2LN1wfX6eDdxdFOi00OfOZvahz',
+            'client_secret': 'test client secret',
+            }
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):

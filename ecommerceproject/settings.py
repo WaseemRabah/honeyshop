1f731ebf5c9d3e39bc60ca9085076692135c29a8
waseem <waseem.96.ra@gmail.com>
2024-02-06 17:00:44 +0100

    Remove some settings comments

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -162,14 +162,6 @@ USE_TZ = True
 
 
 # Cloudinary settings
-"""
-cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-)
-"""
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
     'API_KEY': os.environ.get('API_KEY'),

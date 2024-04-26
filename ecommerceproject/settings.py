100e8f8f11bcef39e944a46e7a5e197a8abc8400
waseem <waseem.96.ra@gmail.com>
2024-04-26 12:36:20 +0200

    Edit databases

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -89,10 +89,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 STATIC_URL = '/static/'

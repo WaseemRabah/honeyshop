c4cb2defd94ebce84fb75ad09f181da15b2e03b0
waseem <waseem.96.ra@gmail.com>
2024-05-05 17:48:12 +0200

    Edit errors handler

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404, handler500
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +18,3 @@ urlpatterns = [
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'ecommerceproject.views.handler404'
-handler500 = 'ecommerceproject.views.handler500'
